98984148895c5886ed9e21d27695781a0dc01314
abderrazak-Bahaj <103391379+abderrazak-Bahaj@users.noreply.github.com>
2023-12-25 12:20:41 +0000

    add packege  Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+
 import { Navbar,Footer } from '@/sections';
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +23,7 @@ export default function RootLayout({
         <Navbar/>
         {children} 
         <SpeedInsights />
+        <Analytics/>
         <Footer/>
       </body>
     </html>
